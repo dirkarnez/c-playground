a3c91cb3587b132a32b67612bfbf3e8d55bb32d8
dirkarnez <smalldirkalex@gmail.com>
2021-10-05 17:29:15 +0800

    - update book.c

--- src/book.c
+++ src/book.c
@@ -37,3 +37,93 @@ void book_free(struct book* self) {
 
 	free(self);
 }
+
+// From https://stackoverflow.com/questions/351733/how-would-one-write-object-oriented-code-in-c
+//
+// struct Animal_Vtable{
+//     typedef void (*Walk_Fun)(struct Animal *a_This);
+//     typedef struct Animal * (*Dtor_Fun)(struct Animal *a_This);
+
+//     Walk_Fun Walk;
+//     Dtor_Fun Dtor;
+// };
+
+// struct Animal{
+//     Animal_Vtable vtable;
+
+//     char *Name;
+// };
+
+// struct Dog{
+//     Animal_Vtable vtable;
+
+//     char *Name; // Mirror member variables for easy access
+//     char *Type;
+// };
+
+// void Animal_Walk(struct Animal *a_This){
+//     printf("Animal (%s) walking\n", a_This->Name);
+// }
+
+// struct Animal* Animal_Dtor(struct Animal *a_This){
+//     printf("animal::dtor\n");
+//     return a_This;
+// }
+
+// Animal *Animal_Alloc(){
+//     return (Animal*)malloc(sizeof(Animal));
+// }
+
+// Animal *Animal_New(Animal *a_Animal){
+//     a_Animal->vtable.Walk = Animal_Walk;
+//     a_Animal->vtable.Dtor = Animal_Dtor;
+//     a_Animal->Name = "Anonymous";
+//     return a_Animal;
+// }
+
+// void Animal_Free(Animal *a_This){
+//     a_This->vtable.Dtor(a_This);
+
+//     free(a_This);
+// }
+
+// void Dog_Walk(struct Dog *a_This){
+//     printf("Dog walking %s (%s)\n", a_This->Type, a_This->Name);
+// }
+
+// Dog* Dog_Dtor(struct Dog *a_This){
+//     // Explicit call to parent destructor
+//     Animal_Dtor((Animal*)a_This);
+
+//     printf("dog::dtor\n");
+
+//     return a_This;
+// }
+
+// Dog *Dog_Alloc(){
+//     return (Dog*)malloc(sizeof(Dog));
+// }
+
+// Dog *Dog_New(Dog *a_Dog){
+//     // Explict call to parent constructor
+//     Animal_New((Animal*)a_Dog);
+
+//     a_Dog->Type = "Dog type";
+//     a_Dog->vtable.Walk = (Animal_Vtable::Walk_Fun) Dog_Walk;
+//     a_Dog->vtable.Dtor = (Animal_Vtable::Dtor_Fun) Dog_Dtor;
+
+//     return a_Dog;
+// }
+
+// int main(int argc, char **argv){
+//     /*
+//       Base class:
+
+//         Animal *a_Animal = Animal_New(Animal_Alloc());
+//     */
+//     Animal *a_Animal = (Animal*)Dog_New(Dog_Alloc());
+
+//     a_Animal->vtable.Walk(a_Animal);
+
+//     Animal_Free(a_Animal);
+// }
