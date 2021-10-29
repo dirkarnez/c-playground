7911c04174a1793d760cd6894ea6489b1f9e936e
Dirk Arnez <smalldirkalex@gmail.com>
2021-10-29 20:40:03 +0800

    - update include/functional.h

--- include/functional.h
+++ include/functional.h
@@ -38,7 +38,31 @@ int add1(int a, int b) {
     return a_added_b_twice_added_a;\
 } 
 
-// Usage
-// 1 + 2 + 2 + 1
-// pretent_to_have_a_callback(1, 2);
+#include <stdio.h>
+
+int main() {
+	// Usage
+	// 1 + 2 + 2 + 1
+	// pretent_to_have_a_callback(1, 2);
+    pretent_to_have_a_callback(7, 9);
+
+    for (int i = 0; i < 10; i++) {
+        int temp = 10;
+        int ans = add3(i);
+        printf("%d -> %d\n", i, ans);
+        /*
+            0 -> 10
+            1 -> 11
+            2 -> 12
+            3 -> 13
+            4 -> 14
+            5 -> 15
+            6 -> 16
+            7 -> 17
+            8 -> 18
+            9 -> 19
+        */
+    }
+}
+
 /******************************/
