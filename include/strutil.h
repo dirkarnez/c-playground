30f1b713963abca7514fd192c3ba9f102b418d3e
Dirk Arnez <smalldirkalex@gmail.com>
2021-11-29 03:22:51 +0800

    - add include/strutil.h

---
+++ include/strutil.h
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+ 
+// int main()
+// {
+//     char a[] = "hello";
+//     char b[] = "world";
+
+//     int length = strlen(a) + strlen(b);
+//     char* newString = (char*)calloc( length + 1 , sizeof(char));
+
+//     strcat(newString, a); 
+//     strcat(newString, b); 
+
+//     printf("%d\n", strlen(newString));
+//     printf("%s\n", newString);
+    
+//    return(0);
+// }
